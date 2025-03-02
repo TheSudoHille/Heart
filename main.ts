ee2cb8ed7830be6caaa7256a90603b6051135b04
TheSudoHille <h.theander@icloud.com>
2025-03-02 08:48:01 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showNumber(0)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    while (false) {
+        for (let index = 0; index <= 4; index++) {
+        	
+        }
+    }
+})
